7ba3d97a88cde0cec6cf746b7b095211d6b021da
MAHESH KUMAR NAYAK <151497328+Maheshna369@users.noreply.github.com>
2024-12-02 15:58:11 +0530

    Add files via upload

---
+++ client/src/components/Login/Login.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useContext } from "react";
+import Header from "../Header";
+import InputLogin from "./InputLogin";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+
+const Login = () => {
+  const [inputLogin, setInputLogin] = useState({ Username: "", Password: "" });
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInputLogin((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+  const handleClick = () => {
+    if (
+      inputLogin.Username.trim() === "" ||
+      inputLogin.Password.trim() === ""
+    ) {
+      alert("Enter Your Username and Password to Login.");
+      return;
+    }
+    axios
+      .post("http://localhost:1000/login", inputLogin)
+      .then((response) => {
+        console.log(
+          `The response while post request while doin login is: ${response}`
+        );
+        sessionStorage.setItem("isAuth", JSON.stringify(true));
+        console.log(`The isAuth is: `, sessionStorage.getItem("isAuth"));
+        navigate(`/weather`);
+        setInputLogin({ Username: "", Password: "" });
+      })
+      .catch((err) =>
+        console.log(`The error while post request during login is: ${err}`)
+      );
+  };
+  return (
+    <>
+      <Header />
+      <InputLogin
+        inputLogin={inputLogin}
+        handleChange={handleChange}
+        handleClick={handleClick}
+      />
+    </>
+  );
+};
+export default Login;
