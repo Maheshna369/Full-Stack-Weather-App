f54dd6012a7e2e894351a35bffc0a64f6a559294
MAHESH KUMAR NAYAK <151497328+Maheshna369@users.noreply.github.com>
2024-12-02 15:51:47 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import env from "dotenv";
+import cors from "cors";
+import connectDB from "./config/config.js";
+import routes from "./routes/routes.js";
+import path, { dirname } from "path";
+const _dirname = path.resolve();
+const app = express();
+env.config();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(_dirname, "/client/dist")));
+const port = process.env.PORT || 1000;
+connectDB();
+app.get("*", (_, res)=>{
+    res.sendFile(path.resolve(_dirname, "client", "dist", "index.html"));
+})
+app.get("/", (req, res) => {
+  res.json({ message: "This is the backend part of weather app." });
+});
+routes(app);
+app.listen(port, () => console.log(`Server is running on port ${port}`));
